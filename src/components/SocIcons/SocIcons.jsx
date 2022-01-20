dbfcef7d3b2bf54f85f7b85014e362e4fc1a7c18
Arman <armmamyan@gmail.com>
2022-01-20 19:48:38 +0400

    build error issue and minor changes

--- src/components/SocIcons/SocIcons.jsx
+++ src/components/SocIcons/SocIcons.jsx
@@ -7,10 +7,10 @@ const SocIcons = () =>{
     return (
         <ul className="soc_icons d-flex">
             <li className="soc_icon">
-                <a href="#" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="Twitter"/></a>
+                <a href="https://twitter.com/CaveCreatures" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="Twitter"/></a>
             </li>
             <li className="soc_icon">
-                <a href="#" target="_blank" rel="noopener noreferrer"><img src={discord} alt="Discord"/></a>
+                <a href="https://discord.gg/hUtQRkqX" target="_blank" rel="noopener noreferrer"><img src={discord} alt="Discord"/></a>
             </li>
         </ul>
     )
