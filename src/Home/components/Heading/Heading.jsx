a7b74fbe39024f2329f3bbf58777820761c1926c
Arman <armmamyan@gmail.com>
2022-01-24 21:54:41 +0400

    fixing alignment issue

--- src/Home/components/Heading/Heading.jsx
+++ src/Home/components/Heading/Heading.jsx
@@ -1,28 +1,33 @@
-import React from 'react'
-import title from "../../../assets/images/logo-2.png"
-import classes from "./Heading.module.css"
+import React from "react";
+import title from "../../../assets/images/logo-2.png";
+import classes from "./Heading.module.css";
 
-
-const Heading = () =>{
-
-    return (
-        <section className={`banner ${classes.banner}`}>
-            <div className="container section_container">
-                <div className="row">
-                    <div className="col-md-12 text-center">
-                        <div className={classes.text_block}>
-                            <h3>COLLECTION</h3>
-                            <img src={title} className={classes.header_title} alt="Calabasas Cave"/>
-                            {/*<p className="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>*/}
-                            <a href="https://discord.gg/hUtQRkqX" target="_blank" rel="noopener noreferrer" className="more_btn">join discord</a>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </section>
-
-    )
+const Heading = () => {
+  return (
+    <section className={`banner ${classes.banner}`}>
+      <div className="container section_container">
+        <div className="row">
+          <div className="col-md-12 text-center d-flex flex-column align-items-center">
+            <h3>COLLECTION</h3>
+            <img
+              src={title}
+              className={classes.header_title}
+              alt="Calabasas Cave"
+            />
+            {/*<p className="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>*/}
+            <a
+              href="https://discord.gg/hUtQRkqX"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn more_btn"
+            >
+              join discord
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Heading;
