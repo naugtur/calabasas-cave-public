8c0b18ee2a56b2ee4c6dc3734d7797793615f065
Arman <armmamyan@gmail.com>
2022-01-28 15:58:06 +0400

    FAQ changes

--- src/Home/components/FaqElements/Faq.jsx
+++ src/Home/components/FaqElements/Faq.jsx
@@ -18,29 +18,26 @@ const Faq = () =>{
         {
             buttonId: "collapseTwo",
             title: "What is Calabasas Cave Creatures?",
-            text: "The first thing to do is download MetaMask, a browser plugin that will act as your interface to the " +
-                "Ethereum Network. You'll also want some ETH, which can be purchased via a fiat on-ramp such as Coinbase " +
-                "or PayPal. From there, you can send the ETH to your MetaMask address and will be able to use any DeFi " +
-                "service that connects to MetaMask! For this specific project, join the discord to be aware of future " +
-                "events and announcements.",
+            text: "Mad Rabbits Riot Club is a collection of 7,777 unique NFTs that are generated randomly using a " +
+                "pre-defined set of traits. Once you own one you'll have the exclusive rights to use the artwork as your" +
+                " digital identity, meaning feel free to use it as your profile picture and express yourself! Later on," +
+                " some special events will occur where you may be able to further the identity of your Cave Creature." +
+                " Calabasas Cave Creatures is also a community of like-minded individuals, with a passion for NFTs " +
+                "in general. Make sure to join the discord to join in on the fun!",
          },
          {
             buttonId: "collapseThree",
             title: "When will Cave Creatures be available to mint? And for how much?",
-            text: "The first thing to do is download MetaMask, a browser plugin that will act as your interface to the " +
-                 "Ethereum Network. You'll also want some ETH, which can be purchased via a fiat on-ramp such as Coinbase " +
-                 "or PayPal. From there, you can send the ETH to your MetaMask address and will be able to use any DeFi " +
-                 "service that connects to MetaMask! For this specific project, join the discord to be aware of future " +
-                 "events and announcements.",
+            text: "The team is aiming for a January 2022 launch. The price of each Creature will be set at 0.06 ETH." +
+                " There is no bonding curve associated with this platform, so all Cave Creatures will cost the same amount.",
          },
          {
             buttonId: "collapseFour",
             title: "What does the team plan on doing after the launch?",
-            text: "The first thing to do is download MetaMask, a browser plugin that will act as your interface to the " +
-                 "Ethereum Network. You'll also want some ETH, which can be purchased via a fiat on-ramp such as Coinbase " +
-                 "or PayPal. From there, you can send the ETH to your MetaMask address and will be able to use any DeFi " +
-                 "service that connects to MetaMask! For this specific project, join the discord to be aware of future " +
-                 "events and announcements.",
+            text: "Post-launch, the team will begin preparing for the Calabasas Cave Creatures’ underground fight club." +
+                " Eventually, all holders will have the option to fight their Cave Creature in a PvP arena, allowing for" +
+                " the chance to earn ETH, build experience, and potentially revealing hidden and sometimes unique extra" +
+                " traits. Be careful though, if you lose, it may cost you some ETH!",
          },
 
     ];
